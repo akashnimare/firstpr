af2528df35e858020db3e8d8bffc76c58d211fe5
akashnimare <svnitakash@gmail.com>
2015-08-13 04:31:54 +0530

    Added files

---
+++ main.js
@@ -0,0 +1,21 @@
+function gitpr($scope, $http) {
+      $scope.getGitInfo = function () {
+         $scope.userNotFound = false;
+         $scope.loaded = false;
+         $http.get("https://api.github.com/search/issues?q=type:pr+author:" + $scope.username + "&sort=created&order=asc&per_page=1" )
+               .success(function (data) {
+                  if (data && data.items && data.items.length) {
+                    $scope.useNotFound = false;
+                    $scope.use = data;
+                    $scope.loaded = true;
+                  }
+                  else {
+                    $scope.useNotFound = true;
+                  }
+               })
+               .error(function () {
+                  $scope.useNotFound = true;
+               });
+        
+      }
+   }
